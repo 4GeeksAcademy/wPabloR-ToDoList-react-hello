7f953ced085fc1ffa566edb30638cf061f1d1575
wPabloR <ricotep0@gmail.com>
2025-06-11 19:28:11 +0000

    styles 1

--- src/js/components/List.jsx
+++ src/js/components/List.jsx
@@ -19,43 +19,48 @@ const List = () => {
     }
 
     const handleKeyDow = (e) => {
-        if (e.key === "Enter"){
+        if (e.key === "Enter") {
             validateInput()
         }
-    } 
+    }
 
     const deleteTask = (indexToDelete) => {
         setTask(tasks.filter((task, index) => index !== indexToDelete))
     }
 
 
+
     return (
-        <div>
-            <h1>To do List</h1>
+        <div style={{ maxWidth: "400px", margin: "auto" }}>
 
-            <input type="text" value={input} onChange={addInput} onKeyDown={handleKeyDow}></input>
-            <button onClick={validateInput}>Add task</button>
 
-            <ul>
-                {tasks.map((task, index) => (
-                    <li
-                        key={index}>{task}
+            <div className="bg-secondary rounded-3 p-2 m-3">
+                <h1>To Do List</h1>
 
-                        <span
-                            style={{ marginLeft: "10px", color: "red", cursor: "pointer" }}
-                            onClick={() => deleteTask(index)}
-                        >
-                            ❌
-                        </span>
-                    </li>
+                <input className="m-auto form-check my-4" type="text" value={input} onChange={addInput} onKeyDown={handleKeyDow}></input>
+                <button className="btn btn-outline-light p-2 mb-4" onClick={validateInput}>Add task</button>
 
-                ))}
-            </ul>
+                <ul style={{ paddingLeft: "20px" }} className="border-top p-3">
+                    {tasks.map((task, index) => (
+                        <li key={index} className="m-3 my-2">
+                            <div className="d-flex justify-content-between align-items-center">
+                            <span className="fs-5">{task}</span>
+                            <button
+                                className="btn p-1"
+                                onClick={() => deleteTask(index)}   
+                            >
+                                ❌
+                            </button>
+                            </div>
+                        </li>
 
-            <h4>{tasks.length} tasks left</h4>
+                    ))}
+                </ul>
+
+                <h4 className="mb-0 border-top p-2 fs-5 text-white">{tasks.length} tasks left</h4>
+            </div>
         </div>
     );
-    ;
-}
+};
 
 export default List
